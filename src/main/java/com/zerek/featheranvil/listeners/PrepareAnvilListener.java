a48f4fe2931b291596f64c8ff1b6292620c69fd6
accLarson <acc.larson@gmail.com>
2022-08-20 14:15:21 -0400

    named lore now contains the name, renamer, and date. Fixed bug where enchanting an item added renamed lore.

--- src/main/java/com/zerek/featheranvil/listeners/PrepareAnvilListener.java
+++ src/main/java/com/zerek/featheranvil/listeners/PrepareAnvilListener.java
@@ -1,22 +1,22 @@
 package com.zerek.featheranvil.listeners;
 
 import com.zerek.featheranvil.FeatherAnvil;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
-import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class PrepareAnvilListener implements Listener {
     private final FeatherAnvil plugin;
     Map<String,String> tagMap = new HashMap<>();
+
 
     public PrepareAnvilListener(FeatherAnvil plugin) {
         this.plugin = plugin;
@@ -25,15 +25,35 @@ public class PrepareAnvilListener implements Listener {
 
     @EventHandler
     public void onPrepareAnvil(PrepareAnvilEvent event){
-        if (event.getResult() != null){
 
-            final String[] stringName = {LegacyComponentSerializer.legacyAmpersand().serialize(event.getResult().displayName()).replaceAll("&[A-fK-o0-9]|\\[|\\]", "")};
+        if (event.getResult() != null && event.getInventory().getFirstItem() != null){
+
+            // 0 - old name | 1 - new name | 2 - new name with role tags stripped
+            final String[] stringNames = new String[3];
+
+            stringNames[0] = LegacyComponentSerializer.legacyAmpersand().serialize(event.getInventory().getFirstItem().displayName()).replaceAll("&[A-fK-o0-9]|\\[|\\]", "").replace("&x","");
+            stringNames[1] = LegacyComponentSerializer.legacyAmpersand().serialize(event.getResult().displayName()).replaceAll("&[A-fK-o0-9]|\\[|\\]", "").replace("&x","");
+            stringNames[2] = stringNames[1].replace("<donor>","").replace("<ass>","").replace("<mod>","");
+
+            if (!stringNames[2].equals(stringNames[0])) {
+
+                String date = new SimpleDateFormat("MMM d yyyy").format(new Date());
+                String playerName = event.getViewers().get(0).getName();
+
+                if (event.getResult().lore() != null) {
+
+                    List<Component> newLore = event.getResult().lore();
+                    newLore.add(Component.text("Named: " + stringNames[2] +  " By: " + playerName + " On: " + date));
+                    event.getResult().lore(newLore);
+                }
+                else event.getResult().lore(Collections.singletonList(Component.text("Named: " + stringNames[2] +  " By: " + playerName + " On: " + date)));
+            }
 
             tagMap.keySet().forEach(k -> {
-                if(stringName[0].startsWith(k) && event.getViewers().get(0).hasPermission("feather.anvil.rename." + k)){
-                    stringName[0] = stringName[0].replace(k, tagMap.get(k));
+                if(stringNames[1].startsWith(k) && event.getViewers().get(0).hasPermission("feather.anvil.rename." + k)){
+                    stringNames[1] = stringNames[1].replace(k, tagMap.get(k));
                     ItemMeta resultMeta = event.getResult().getItemMeta();
-                    resultMeta.displayName(MiniMessage.miniMessage().deserialize(stringName[0]));
+                    resultMeta.displayName(MiniMessage.miniMessage().deserialize(stringNames[1]));
                     if (!PlainTextComponentSerializer.plainText().serialize(resultMeta.displayName()).trim().isEmpty()) event.getResult().setItemMeta(resultMeta);
                 }
             });
